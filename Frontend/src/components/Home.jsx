06f7896737e0d95eeb4a4b71d48a8027cc4b4b3e
Pukar77 <pukarrimal11@gmail.com>
2025-05-26 09:47:16 +0545

    feat: implement HeroSection and CategoryCarusel components in Home

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./shared-component/Navbar";
+import HeroSection from "./HeroSection";
+import CategoryCarusel from "./CategoryCarusel";
 
 function Home() {
   return (
     <div>
       <Navbar />
-      
+      <div className="mt-3">
+        <HeroSection />
+        <CategoryCarusel />
+      </div>
     </div>
   );
 }
